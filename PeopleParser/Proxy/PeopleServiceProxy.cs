ef202c040b13c373215d5063a10a5cd817ff77d6
Rakesh <rakeshl84@outlook.com>
2018-03-18 11:42:44 +0530

    disposing the httpClient

--- PeopleParser/Proxy/PeopleServiceProxy.cs
+++ PeopleParser/Proxy/PeopleServiceProxy.cs
@@ -12,17 +12,19 @@ namespace PeopleParser.Proxy
     {
         public async Task<IEnumerable<People>> GetAll()
         {
-            var client = new HttpClient();
-            var url = ConfigurationManager.AppSettings["PeopleServiceUrl"].ToString();
-            client.BaseAddress = new Uri(url);
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept
-                .Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            using (var client = new HttpClient())
+            {
+                var url = ConfigurationManager.AppSettings["PeopleServiceUrl"].ToString();
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept
+                    .Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
-            var res = await client.GetAsync("people.json");
-            res.EnsureSuccessStatusCode();
-            var response = await res.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<IEnumerable<People>>(response);
+                var res = await client.GetAsync("people.json");
+                res.EnsureSuccessStatusCode();
+                var response = await res.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IEnumerable<People>>(response);
+            }
         }
     }
 }
